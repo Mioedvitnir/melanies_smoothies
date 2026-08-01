6375531a90a1656db0f2ba0d6acdc910e8e9c772
Miödvitnir <62642527+Mioedvitnir@users.noreply.github.com>
2026-08-01 10:48:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,15 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into  smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
   
     #st.write(my_insert_stmt)
-    st.subheader(fruit_chosen + ' Nutrition Information')
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+    
     #st.text(smoothiefroot_response.json())
     st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
     # Write directly to the app
